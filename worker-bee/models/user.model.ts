477e311c500aa00e69f24c7e0c0be7248cb6ed37
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-03 21:41:17 -0800

    updated user interface

--- worker-bee/models/user.model.ts
+++ worker-bee/models/user.model.ts
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
-    hives: { location: [] },
+    hives: {},
     schedule: [],
   },
   { strict: true }
--- worker-bee/src/pages/api/User/userServices.ts
+++ worker-bee/src/pages/api/User/userServices.ts
@@ -15,7 +15,7 @@ export default async function handler(
       await dbConnect();
       const response = await UserSchema.find(
         { email: req.query.email },
-        { _id: 1, username: 1 }
+        { _id: 1, username: 1, hives: 1, schedule: 1 }
       );
       if (response) {
         return res.status(200).json({ response });
@@ -35,6 +35,42 @@ export default async function handler(
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
+          hives: {
+            Pasadena: [
+              {
+                name: "hive1",
+                weight: 100,
+                queenPlaced: "03-03-2023",
+                temperament: 5,
+                medicine: ["med1", "med2"],
+                disease: ["disease1", "disease2"],
+                location: [0, 0],
+              },
+              {
+                name: "hive2",
+                weight: 100,
+                queenPlaced: "03-04-2023",
+                temperament: 5,
+                medicine: ["med1", "med2"],
+                disease: ["disease1", "disease2"],
+                location: [0, 0],
+              },
+              {
+                name: "hive3",
+                weight: 100,
+                queenPlaced: "03-05-2023",
+                temperament: 5,
+                medicine: ["med1", "med2"],
+                disease: ["disease1", "disease2"],
+                location: [0, 0],
+              },
+            ],
+          },
+          schedule: {
+            hive1: { meds: "03-03-2023" },
+            hive2: { meds: "03-04-2023" },
+            hive3: { meds: "03-05-2023" },
+          },
         });
         try {
           const result = await user.save();
