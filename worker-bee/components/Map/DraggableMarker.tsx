fdb326657cc7bc5490b0eaaf7d2206557113653d
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-03 21:40:53 -0800

    updated static map components

--- worker-bee/components/Map/DraggableMarker.tsx
+++ worker-bee/components/Map/DraggableMarker.tsx
@@ -1,24 +1,35 @@
 import { LeafletMouseEvent } from "leaflet";
-import { useState, useRef, useMemo } from "react";
+import { useState, useRef, useMemo, useEffect } from "react";
 import { useMapEvents, Marker } from "react-leaflet";
 import BeeIcon from "./BeeIcon";
-import { Hive } from "./mapInterface";
+import { Ref } from "./mapInterface";
 
-const DraggableMarker: React.FC<Hive> = ({ location }) => {
+const DraggableMarker: React.FC<Ref> = ({ location }) => {
   const [position, setPosition] = useState({ lat: 0, lng: 0 });
-  const markerRef = useRef(null);
+  const markerRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (location !== null) {
+      location.current = [position.lat, position.lng];
+    }
+  }, [position]);
+
   const eventHandlers = useMemo(
     () => ({
       dragend() {
         const marker = markerRef.current;
         if (marker != null) {
-          console.log(marker);
-          //   setPosition({lat: marker.getLatLng().lat, lng: marker.getLatLng().lng});
+          console.log(marker.getLatLng());
+          setPosition({
+            lat: marker.getLatLng().lat,
+            lng: marker.getLatLng().lng,
+          });
         }
       },
     }),
     []
   );
+
   useMapEvents({
     click(e: LeafletMouseEvent) {
       setPosition({ lat: e.latlng.lat, lng: e.latlng.lng });
--- worker-bee/components/Map/FlyToButton.tsx
+++ worker-bee/components/Map/FlyToButton.tsx
@@ -3,9 +3,16 @@ import { Hive } from "./mapInterface";
 
 const FlyToButton: React.FC<Hive> = ({ location, name }) => {
   const map = useMap();
+  if (!location) {
+    return null;
+  }
   if (location.length > 0) {
     const clickHandler = () => map.flyTo(location, 15);
-    return <button onClick={clickHandler}>{name}</button>;
+    return (
+      <button className="btn btn-light btn-sm" onClick={clickHandler}>
+        {name}
+      </button>
+    );
   } else return null;
 };
 
