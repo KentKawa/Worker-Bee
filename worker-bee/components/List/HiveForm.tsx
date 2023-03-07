ba3cfa0bacab6f59b4334bb6fb6d514c17a9159a
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-06 22:17:29 -0800

    Update HiveForm.tsx

--- worker-bee/components/List/HiveForm.tsx
+++ worker-bee/components/List/HiveForm.tsx
@@ -5,9 +5,11 @@ import React, {
   ChangeEventHandler,
 } from "react";
 import { User } from "../Map/mapInterface";
+import axios from "axios";
 //COMPONENTS
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 import MapDynamic from "../Map/MapDynamicNoSSR";
 import Image from "next/image";
 //STYLE
@@ -24,8 +26,8 @@ import Pin from "../../public/map-location-svgrepo-com.png";
 import Meds from "../../public/form/meds.png";
 import Disease from "../../public/form/disease.png";
 
-const HiveForm: React.FC<User> = ({ hives, _id }) => {
-  const [hiveCity, setHiveCity] = useState(""),
+const HiveForm: React.FC<User> = ({ hives, _id, setUser }) => {
+  const [apiaryName, setApiaryName] = useState("Select an Apiary"),
     [hiveName, setHiveName] = useState(""),
     [weight, setWeight] = useState<number>(),
     [queenPlaced, setQueenPlaced] = useState(""),
@@ -33,6 +35,10 @@ const HiveForm: React.FC<User> = ({ hives, _id }) => {
     [medicine, setMedicine] = useState<string[]>([]),
     [disease, setDisease] = useState<string[]>([]);
   const location = useRef<[number, number]>([0, 0]);
+  let apiarys: string[] = [];
+  if (hives) {
+    apiarys = Object.keys(hives);
+  }
 
   const temperamentName = () => {
     if (temperament <= 2) {
@@ -73,6 +79,7 @@ const HiveForm: React.FC<User> = ({ hives, _id }) => {
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     console.log(
+      apiaryName,
       hiveName,
       weight,
       queenPlaced,
@@ -81,27 +88,57 @@ const HiveForm: React.FC<User> = ({ hives, _id }) => {
       medicine,
       disease
     );
-    // const response = fetch(`CHANGE ME TO RIGHT API`, {
-    //   hiveName,
-    //   weight,
-    //   queenPlaced,
-    //   location: location.current,
-    //   temperament,
-    //   medicine,
-    //   disease,
-    // });
-    // setHiveName("");
-    // setWeight(0);
-    // setQueenPlaced("");
-    // setTemperament(5);
-    // setMedicine([""]);
-    // setDisease([""]);
-    // location.current = [];
+
+    const response = axios
+      .put(`http://localhost:3000/api/Hive/createHive?_id=${_id}`, {
+        apiaryName,
+        hiveName,
+        weight,
+        queenPlaced,
+        location: location.current,
+        temperament,
+        medicine,
+        disease,
+      })
+      .then((res) => {
+        console.log(res);
+        if (setUser) {
+          setUser((prev) => ({ ...prev, hives: res.data.hives }));
+          setHiveName("");
+          setWeight(0);
+          setQueenPlaced("");
+          setTemperament(5);
+          setMedicine([]);
+          setDisease([]);
+          location.current = [0, 0];
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className={style.formContainer}>
       <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>Apiary</Form.Label>
+          <Dropdown>
+            <Dropdown.Toggle variant="light">{apiaryName}</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {apiarys.map((ele) => {
+                return (
+                  <Dropdown.Item
+                    onClick={() => setApiaryName(`${ele}`)}
+                    key={ele}
+                  >
+                    {ele}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Form.Group>
         <Form.Group>
           <Form.Label>
             <Image src={Honey} alt="honey" height={20} /> Name
@@ -122,7 +159,7 @@ const HiveForm: React.FC<User> = ({ hives, _id }) => {
             type="number"
           />
         </Form.Group>
-        <Form.Group>
+        <Form.Group style={{ display: "flex", flexDirection: "column" }}>
           <Form.Label>
             <Image src={Crown} alt="honey" height={20} /> Date of Queen
             placement
