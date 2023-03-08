6cb44b7e7b1e5e661612b9fe6da40e4af6b70445
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-07 22:55:31 -0800

    Update user.model.ts

--- worker-bee/models/user.model.ts
+++ worker-bee/models/user.model.ts
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
+export const Hive = new mongoose.Schema({
+  _id: { type: mongoose.Types.ObjectId, auto: true },
+  hiveName: { type: String, required: true },
+  weight: { type: Number, required: true },
+  queenPlaced: { type: String, required: true },
+  temperament: { type: Number, required: true },
+  medicine: { type: [String], default: [] },
+  disease: { type: [String], default: [] },
+  location: { type: [Number], default: [0, 0] },
+});
+
+export const UserSchema = new mongoose.Schema(
   {
     firstName: { type: String, require: true },
     lastName: String,
     username: { type: String, required: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
-    hives: {},
+    hives: {
+      type: Map,
+      of: [Hive],
+      default: {},
+    },
     schedule: [],
   },
   { strict: true }
