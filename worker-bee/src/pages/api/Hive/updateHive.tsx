0797bb0353c9ddcbf1459ddb1b1f5b49dbd33442
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-08 22:39:47 -0800

    Update updateHive.tsx

--- worker-bee/src/pages/api/Hive/updateHive.tsx
+++ worker-bee/src/pages/api/Hive/updateHive.tsx
@@ -1,35 +1,60 @@
 import dbConnect from "services/mongo";
 import { NextApiRequest, NextApiResponse } from "next";
 import UserSchema from "models/user.model";
+import { ObjectId } from "bson";
+import mongoose from "mongoose";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   await dbConnect();
-  const options = { new: true, setDefaultsOnInsert: true, upsert: true };
-  const response = UserSchema.findOneAndUpdate(
-    { [`hives.${req.body.apiaryName}.name`]: req.body.hiveName },
-    {
-      $set: {
-        [`hives.${req.body.apiaryName}.$.hiveName`]: req.body.hiveName,
-        [`hives.${req.body.apiaryName}.$.weight`]: req.body.weight,
-        [`hives.${req.body.apiaryName}.$.queenPlaced`]: req.body.queenPlaced,
-        [`hives.${req.body.apiaryName}.$.location`]: req.body.location,
-        [`hives.${req.body.apiaryName}.$.temperament`]: req.body.temperament,
-        [`hives.${req.body.apiaryName}.$.medicine`]: req.body.medicine,
-        [`hives.${req.body.apiaryName}.$.disease`]: req.body.disease,
-      },
-    },
-    options,
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(400).json(err);
+  const findId = UserSchema.findById(req.query._id)
+    .then((user) => {
+      const hive = user.hives.get(req.body.apiaryName);
+      const hiveId = new ObjectId(req.body._id);
+      const hiveIndex = hive.findIndex(
+        (obj: { _id: { equals: (arg0: ObjectId) => any } }) =>
+          obj._id.equals(hiveId)
+      );
+      if (hiveIndex >= 0) {
+        const newHive = {
+          _id: hiveId,
+          hiveName: req.body.hiveName,
+          weight: req.body.weight,
+          queenPlaced: req.body.queenPlaced,
+          temperament: req.body.temperament,
+          medicine: req.body.medicine,
+          disease: req.body.disease,
+          location: req.body.location,
+        };
+        user.hives.set(req.body.apiaryName, [
+          ...hive.slice(0, hiveIndex),
+          newHive,
+          ...hive.slice(hiveIndex + 1),
+        ]);
+        user
+          .save()
+          .then((results: any) => {
+            console.log(
+              `Updated Apiary ${req.body.apiaryName}: Hive ${hiveId}`
+            );
+            res.status(200).json({ results });
+          })
+          .catch((err: any) => {
+            console.log(
+              `Error updating Apiary ${req.body.apiaryName}: Hive ${hiveId}`,
+              err
+            );
+            res.status(400).json({ err });
+          });
       } else {
-        console.log("Updated Hives:", results);
-        return res.status(200).json(results);
+        console.log("OBJECT NOT FOUND");
+        res.status(404).json({ data: "Object not found" });
       }
-    }
-  );
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).end();
+    });
 }
