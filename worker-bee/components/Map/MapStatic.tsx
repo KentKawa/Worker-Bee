e9253f0449874f78101a3e85cd1caf3d6ec6a2fc
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-10 21:07:46 -0800

    Update MapStatic.tsx

--- worker-bee/components/Map/MapStatic.tsx
+++ worker-bee/components/Map/MapStatic.tsx
@@ -1,69 +1,15 @@
 import React, { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import { User } from "./mapInterface";
+//COMPONENTS
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
 import FlyToButton from "./FlyToButton";
-import BeeIcon from "./BeeIcon";
-import { relative } from "path";
+import PopupMarkers from "./PopUpMarkers";
+import FlyToList from "./FlyToList";
+//STYLES
+import "leaflet/dist/leaflet.css";
 
 const Map: React.FC<User> = ({ hives }) => {
-  const getHiveNames = () => {
-    if (hives) {
-      const name = Object.keys(hives);
-      console.log(name);
-      return (
-        <>
-          {name.map((key) => {
-            return (
-              <DropdownButton
-                style={{ zIndex: 500, justifySelf: "flex-end" }}
-                title={key}
-                key={key}
-              >
-                {hives[key].map((ele) => {
-                  return (
-                    <div style={{ position: "relative" }} key={ele.name}>
-                      <FlyToButton location={ele.location} name={ele.name} />
-                    </div>
-                  );
-                })}
-              </DropdownButton>
-            );
-          })}
-        </>
-      );
-    }
-  };
-
-  const placeMarkers = () => {
-    if (hives) {
-      const name = Object.keys(hives);
-      for (let key of name) {
-        return (
-          <>
-            {hives[key].map((ele) => {
-              return (
-                <Marker
-                  key={`${ele.name}/${key}`}
-                  title={ele.name}
-                  icon={BeeIcon}
-                  position={ele.location}
-                >
-                  <Popup>
-                    <p>{ele.name}</p> <hr />
-                    <p>WT:{ele.weight}</p>
-                  </Popup>
-                </Marker>
-              );
-            })}
-          </>
-        );
-      }
-    }
-  };
-
   return (
     <MapContainer
       style={{ height: "100%", width: "100%" }}
@@ -76,10 +22,17 @@ const Map: React.FC<User> = ({ hives }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors<br/><a href="https://www.flaticon.com/free-icons/bee" title="bee icons">Bee icons created by Freepik - Flaticon</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
-        {getHiveNames()}
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-end",
+        }}
+      >
+        <FlyToList hives={hives} />
       </div>
-      {placeMarkers()}
+      <PopupMarkers hives={hives} />
     </MapContainer>
   );
 };
