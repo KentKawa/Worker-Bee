558108f8d961ec147b3ac68d77229edb9370696d
KentKawa <112824822+KentKawa@users.noreply.github.com>
2023-03-10 21:07:33 -0800

    Update FlyToButton.tsx

--- worker-bee/components/Map/FlyToButton.tsx
+++ worker-bee/components/Map/FlyToButton.tsx
@@ -1,16 +1,25 @@
 import { useMap } from "react-leaflet";
-import { Hive } from "./mapInterface";
+import { User } from "./mapInterface";
 
-const FlyToButton: React.FC<Hive> = ({ location, name }) => {
+interface FlyTo {
+  location: [number, number];
+  hiveName: string;
+}
+
+const FlyToButton: React.FC<FlyTo> = ({ location, hiveName }) => {
   const map = useMap();
   if (!location) {
     return null;
   }
   if (location.length > 0) {
-    const clickHandler = () => map.flyTo(location, 15);
+    const clickHandler = () => map.flyTo(location, 18);
     return (
-      <button className="btn btn-light btn-sm" onClick={clickHandler}>
-        {name}
+      <button
+        className="btn btn-light btn-sm"
+        style={{ width: "100%" }}
+        onClick={clickHandler}
+      >
+        {hiveName}
       </button>
     );
   } else return null;
